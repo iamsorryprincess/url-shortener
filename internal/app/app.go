bc0c55b2fc53e8681b184985a84b6fbe8c2ed926
iamsorryprincess <nickpank112@yandex.ru>
2022-05-24 16:56:25 +0300

    db ping

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,12 @@
 package app
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/chi/v5"
@@ -13,12 +16,14 @@ import (
 	"github.com/iamsorryprincess/url-shortener/internal/service"
 	"github.com/iamsorryprincess/url-shortener/internal/storage"
 	"github.com/iamsorryprincess/url-shortener/pkg/hash"
+	_ "github.com/jackc/pgx/stdlib"
 )
 
 type Configuration struct {
-	Address     string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	StoragePath string `env:"FILE_STORAGE_PATH"`
+	Address            string `env:"SERVER_ADDRESS" envDefault:":8080"`
+	BaseURL            string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	StoragePath        string `env:"FILE_STORAGE_PATH"`
+	DbConnectionString string `env:"DATABASE_DSN" envDefault:""`
 }
 
 func parseConfiguration() (*Configuration, error) {
@@ -32,6 +37,7 @@ func parseConfiguration() (*Configuration, error) {
 	flag.StringVar(&configuration.Address, "a", configuration.Address, "server address")
 	flag.StringVar(&configuration.BaseURL, "b", configuration.BaseURL, "base url")
 	flag.StringVar(&configuration.StoragePath, "f", configuration.StoragePath, "file storage path")
+	flag.StringVar(&configuration.DbConnectionString, "d", configuration.DbConnectionString, "db connection string")
 	flag.Parse()
 	return configuration, nil
 }
@@ -67,6 +73,27 @@ func Run() {
 		return
 	}
 
+	if configuration.DbConnectionString == "" {
+		log.Fatal("empty db connection string")
+		return
+	}
+
+	db, err := sql.Open("pgx", configuration.DbConnectionString)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer db.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	r := chi.NewRouter()
 
 	r.Use(chimiddleware.Logger)
@@ -78,6 +105,18 @@ func Run() {
 	r.Post("/api/shorten", handlers.JSONMakeShortURLHandler(urlService, configuration.BaseURL))
 	r.Get("/{URL}", handlers.GetFullURLHandler(urlService))
 	r.Get("/api/user/urls", handlers.GetUserUrls(urlService))
+
+	r.Get("/ping", func(writer http.ResponseWriter, request *http.Request) {
+		err := db.PingContext(request.Context())
+
+		if err != nil {
+			log.Println(err)
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		writer.WriteHeader(http.StatusOK)
+	})
 
 	log.Fatal(http.ListenAndServe(configuration.Address, r))
 }
---
+++ scripts/db.sh
@@ -0,0 +1,1 @@
+docker run -d -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_DB=dbname -p 5432:5432 postgres:14.3-alpine
